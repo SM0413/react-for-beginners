8eaa2e4fbb898f1cedf55a09123c9c5828a449d3
SM0413 <kbjtmdals@naver.com>
2022-08-11 08:18:34 +0900

    컴포넌트를 써서 화면에 출력 & 라우터 생성

--- src/App.js
+++ src/App.js
@@ -1,43 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        movies.map((moive) => (
-          <div key={moive.id}>
-            <img alt="" src={moive.medium_cover_image} />
-            <h2>{moive.title}</h2>
-            <p>{moive.summary}</p>
-            <ul>
-              <li>
-                {moive.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </li>
-            </ul>
-          </div>
-        ))
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
