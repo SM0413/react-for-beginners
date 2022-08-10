d93365bcae3f011bd384c1c90293f6f95d62e1f5
SM0413 <kbjtmdals@naver.com>
2022-08-11 07:41:26 +0900

    영화 API를 화면에 뿌리는 방법

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json()).then((json) => setCoins(json));
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
     setLoading(false);
-  }, [])
-return (
-  
-  <div>
-    <h1>The Coins! ({coins.length})</h1>
-    {loading ? <strong>Loading....</strong> : 
-    <ul>
-      {coins.map((coin) => <li key ={coin.id}>{coin.name}({coin.symbol}): {coin.quotes.USD.price} USD</li>)}
-    </ul>}
-  </div>
-);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        movies.map((moive) => (
+          <div key={moive.id}>
+            <img alt="" src={moive.medium_cover_image} />
+            <h2>{moive.title}</h2>
+            <p>{moive.summary}</p>
+            <ul>
+              <li>
+                {moive.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </li>
+            </ul>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default App;
